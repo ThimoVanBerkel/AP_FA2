0a5aeffe3b12205e1ea350bb74284d282b4b0b7c
ThimoVanBerkel <thimo.vanberkel@student.hu.nl>
2023-05-22 09:41:10 +0200

    Beginning, added hpp classes
    Made the all the cpp and hpp files, added the right data to the hpp files

---
+++ warehouse/src/include/warehouse.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+
+#include "employee.hpp"
+#include "shelf.hpp"
+
+class Warehouse
+{
+private:
+    std::vector<Employee> Employees;
+public:
+
+    Warehouse();
+    void addEmployee(Employee employee);
+};
