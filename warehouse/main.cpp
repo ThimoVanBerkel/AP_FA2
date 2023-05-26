ff8072957b71817438a065b223651fb639e83e23
ThimoVanBerkel <thimo.vanberkel@student.hu.nl>
2023-05-26 18:31:28 +0200

    Main added
    Main added with a small demo

--- warehouse/main.cpp
+++ warehouse/main.cpp
@@ -7,70 +7,71 @@
 int main(void)
 {
     std::string enter;
-
-    std::cout << "Welkom to the Demo for the warehouse Fa2" << std::endl;
-    std::cout << "We shall be making a warehouse first with the following values" << std::endl;
+    std::cout << "Welkom bij mijn demo van FA2 van adaptive programming." << std::endl;
+    std::cout << "Eerst maken we een warenhuis aan en voegen daarna er waardes aan toe." << std::endl;
+    std::cout << "Dit doen we om een goede presentatie te maken van de demo." << std::endl;
     std::cout << std::endl;
 
     Warehouse warehouse = Warehouse();
     Shelf shelf1 = Shelf();
     shelf1.pallets = {
-        Pallet("Books", 100, 10),
-        Pallet("Bikes", 100, 80),
-        Pallet("Bikes", 100, 2),
-        Pallet("CellPhones", 100, 55)};
+        Pallet("Barbie", 100, 15),
+        Pallet("Pokemon", 100, 80),
+        Pallet("Pokemon", 100, 2),
+        Pallet("Lego", 100, 55)};
     Shelf shelf2 = Shelf();
     shelf2.pallets = {
-        Pallet("Books", 100, 20),
-        Pallet("Candys", 100, 40),
-        Pallet("Bikes", 100, 30),
-        Pallet("Books", 100, 10)};
+        Pallet("Barbie", 100, 20),
+        Pallet("Games", 100, 40),
+        Pallet("Pokemon", 100, 30),
+        Pallet("Barbie", 100, 10)};
 
     warehouse.addShelf(shelf1);
     warehouse.addShelf(shelf2);
-    warehouse.addEmployee(Employee("Bob", true));
+    warehouse.addEmployee(Employee("Karel", true));
 
     for (int i = 0; i < 2; i++)
     {
-        // loop
         std::cout << "Shelf " << i << std::endl;
         for (int j = 0; j < 4; j++)
         {
-            // Test for every pallet if it meets the requirments
-            // REQUIRE(warehouse.shelves[i].pallets[j].getItemCount() == 0);
-            std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
+            std::cout << "- Pallet: " << j << " item = " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
         }
     }
-    std::cout << "We will also add a employee to work in our warehous and he is certified How Nice !" << std::endl;
-    std::cout << "Next up we shall rearange both shelves | Pres enter to continue" << std::endl;
-
-    std::getline(std::cin, enter);
-    std::cin.clear();
-
-    std::cout << "Now you may not have seen it but we shall print them out so you can see | Pres enter to continue." << std::endl;
-    warehouse.rearrangeShelf(warehouse.Shelves[0]);
-    warehouse.rearrangeShelf(warehouse.Shelves[1]);
-    std::getline(std::cin, enter);
-    std::cin.clear();
     std::cout << std::endl;
-
-    for (int i = 0; i < 2; i++)
-    {
-        // loop
-        std::cout << "Shelf " << i << std::endl;
-        for (int j = 0; j < 4; j++)
-        {
-
-            std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
-        }
-    }
-
-    std::cout << "Now lets say some one wants to buy 40 books." << std::endl;
-    std::cout << "This would in our case be everyting" << std::endl;
-    std::cout << "Now lets show if all the pallets with books have gone to zero | Pres enter to continue" << std::endl;
+    std::cout << "We voegen nu een een medewerker mee met een certificaat" << std::endl;
+    std::cout << "Medewerker toegevoegd: " << warehouse.Employees[0].getName() << std::endl;
+    std::cout << "Laten we nu een aantal dingen testen | Druk op enter om door te gaan." << std::endl;
 
     std::getline(std::cin, enter);
+
     std::cin.clear();
+
+    // std::cout << "Now you may not have seen it but we shall print them out so you can see | Pres enter to continue." << std::endl;
+    // std::getline(std::cin, enter);
+    // warehouse.rearrangeShelf(warehouse.Shelves[0]);
+    // warehouse.rearrangeShelf(warehouse.Shelves[1]);
+    
+    // std::cin.clear();
+    // std::cout << std::endl;
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     // loop
+    //     std::cout << "Shelf " << i << std::endl;
+    //     for (int j = 0; j < 4; j++)
+    //     {
+
+    //         std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
+    //     }
+    // }
+
+    // std::cout << "Now lets say some one wants to buy 40 books." << std::endl;
+    // std::cout << "This would in our case be everyting" << std::endl;
+    // std::cout << "Now lets show if all the pallets with books have gone to zero | Pres enter to continue" << std::endl;
+
+    // std::getline(std::cin, enter);
+    // std::cin.clear();
 
     warehouse.pickItems("Books", 40);
 
@@ -81,42 +82,40 @@ int main(void)
         for (int j = 0; j < 4; j++)
         {
 
-            std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
+            std::cout << "- Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
         }
     }
 
-    std::cout << "Now lets say we want to add 1 item to every pallet we can also do that" << std::endl;
-    std::cout << "Pres enter to continue" << std::endl;
+    std::cout << "We kunnen ook de pallets aanvullen zoals we nu gaan laten zien. " << std::endl;
+    std::cout << "Druk op enter voor de aanvulling van de producten" << std::endl;
     std::getline(std::cin, enter);
     std::cin.clear();
 
     for (int i = 0; i < 2; i++)
     {
-        // loop
         std::cout << "Shelf " << i << std::endl;
         for (int j = 0; j < 4; j++)
         {
             warehouse.Shelves[i].pallets[j].putOne();
-            std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
+            std::cout << "- Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
         }
     }
 
-    std::cout << "Now we can also take them away again: Pres enter to continue" << std::endl;
+    std::cout << "Nu kunnen we ook er weer producten er van af trekken." << std::endl;
     std::getline(std::cin, enter);
     std::cin.clear();
 
     for (int i = 0; i < 2; i++)
     {
-        // loop
         std::cout << "Shelf " << i << std::endl;
         for (int j = 0; j < 4; j++)
         {
             warehouse.Shelves[i].pallets[j].takeOne();
-            std::cout << "-->Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
+            std::cout << "- Pallet: " << j << " item: " << warehouse.Shelves[i].pallets[j].getItemName() << " Amount: " << warehouse.Shelves[i].pallets[j].getItemCount() << std::endl;
         }
     }
 
     std::cout << std::endl;
-    std::cout << "This whas a small demo on what we can do with the functions written in this project" << std::endl;
+    std::cout << "Dit was een korte demo over dat de aplicatie werkt naar behoren." << std::endl;
     std::cout << std::endl;
 }
